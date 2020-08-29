725160acd0e82b9ae663f7d659be504c9d6b1bcc
Pavel Slama <info@pavelslama.cz>
2020-08-29 17:07:57 +0200

    Revert "set invalid result in case of failure"
    This reverts commit b1e52afb7f157c6dc1de670dfe6043e1aa9c5301.

--- SHTC3.cpp
+++ SHTC3.cpp
@@ -130,12 +130,6 @@ bool SHTC3::read(uint16_t &temp, uint16_t &humidity, bool low_power) {
     temp = (data[3] << 8) | data[4];
 
 END:
-
-    // in case of failure, set invalid result
-    if (!ret) {
-        temp = humidity = USHRT_MAX;
-    }
-
     // sleep now
     sendCmd(CMD_SLEEP);
     return ret;
